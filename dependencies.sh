067f3220152482e4b53cacb6c816a8ec4bb416a4
xiao <xiao@altohealth.io>
2024-02-28 21:28:34 +0000

    Update some of the information

---
+++ dependencies.sh
@@ -0,0 +1,63 @@
+cd /var/www/html
+Git clone https://github.com/xiao11lam/Alto-Health.git
+
+# give the full privilege on the folder
+sudo chown -R Alto-Health/
+
+############################# Install the Dependencies now ####################
+# update
+sudo apt update
+
+
+# install apache2
+sudo apt install apache2 -y
+
+
+# install MySQL
+sudo apt install mysql-server -y
+
+
+# Install PHP
+sudo apt install php libapache2-mod-php -y
+
+
+# Install PHP and MySQL package
+sudo apt install php-mysql -y
+
+
+# check the apache2 status, and get to the localhost if shows the page then it works
+service apache2 status
+
+# Install vim
+sudo apt install vim -y
+
+# change the default render page path, DocumentRoot should be like /var/www/html/Alto-Health
+vim /etc/apache2/sites-available/000-default.conf
+
+# Reload the apache2
+sudo service apache2 reload
+
+
+# create DB
+create database learnify;
+
+# Load the SQL scripts to the DB
+mysql -h localhost -u root -p learnify < ./database/learnify.sql
+
+# if show  Access denied for user 'root'@'localhost'
+sudo mysql
+
+	SELECT host,user,authentication_string,plugin FROM mysql.user;
+
+	ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '';
+
+	ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
+
+	FLUSH PRIVILEGES;
+
+	exit;
+
+
+
+
+
